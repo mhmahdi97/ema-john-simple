a001eee799d5292054f990e414cc0737de8c5132
mhmahdi97 <mhmahdi97@gmail.com>
2023-04-09 23:52:58 +0600

    created ordered card

--- src/loaders/cartProductsLoader.js
+++ src/loaders/cartProductsLoader.js
@@ -6,13 +6,19 @@ const cartProductsLoader = async () => {
     
     // if cart data is in database, will have to use async await
     const storedCart = getShoppingCart();
+    const savedCart = [];
     
     console.log(storedCart);
     for (const id in storedCart) {
         const addedProduct = products.find(pd => pd.id === id)
+        if (addedProduct) {
+            const quantity = storedCart[id];
+            addedProduct.quantity = quantity;
+            savedCart.push(addedProduct);
+        }  
     }
-    
-    return products;
+    // return [products, savedCart];
+    return savedCart;
 };
 
 export default cartProductsLoader;
